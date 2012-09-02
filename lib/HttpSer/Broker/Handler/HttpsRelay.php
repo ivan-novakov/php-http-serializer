225761a829a9d11a5c5d68a54609cdea03ce532c
Ivan Novakov <ivan.novakov@debug.cz>
2012-09-02 18:32:50 +0200

    https relay broker handler with verify_peer

---
+++ lib/HttpSer/Broker/Handler/HttpsRelay.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace HttpSer\Broker\Handler;
+
+
+class HttpsRelay extends AbstractHandler
+{
+
+    /**
+     * HTTP client object.
+     *
+     * @var \Zend\Http\Client
+     */
+    protected $_client = NULL;
+
+
+    public function process ($data)
+    {
+        $request = unserialize($data);
+        if (FALSE === $request || ! ($request instanceof \Zend\Http\Request)) {
+            // error
+        }
+        
+        $request->setUri($this->_config->targetUrl);
+        $request->getHeaders()
+            ->addHeaderLine('Connection: keep-alive');
+        
+        $client = $this->getClient();
+        
+        $response = $client->send($request);
+        
+        return serialize($response);
+    }
+
+
+    /**
+     * Sets the HTTP client object.
+     *
+     * @param \Zend\Http\Client $client            
+     */
+    public function setClient (\Zend\Http\Client $client)
+    {
+        $this->_client = $client;
+    }
+
+
+    /**
+     * Returns the HTTP client object
+     *
+     * @return \Zend\Http\Client
+     */
+    public function getClient ()
+    {
+        if (! $this->_client) {
+            $clientConfig = $this->_config->client;
+            $client = new \Zend\Http\Client(NULL, $clientConfig->options);
+            
+            $adapterConfig = $clientConfig->adapter;
+            $adapterClass = $adapterConfig->class;
+            
+            $adapter = new $adapterClass();
+            $adapter->setOptions($adapterConfig->options);
+            $adapter->setStreamContext($adapterConfig->streamContext->toArray());
+            
+            $client->setAdapter($adapter);
+            
+            $this->setClient($client);
+        }
+        
+        return $this->_client;
+    }
+}
--- tmp/broker.php
+++ tmp/broker.php
@@ -6,52 +6,76 @@ use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 require 'common.php';
 
-$globalConfig = new \Zend\Config\Config(array(
-    'broker' => array(
-        'connection' => array(
-            'host' => 'localhost', 
-            'port' => 5672, 
-            'user' => 'mcu', 
-            'password' => 'mcuapi', 
-            'vhost' => '/mcu'
-        ), 
-        
-        'bindings' => array(
-            'exchange' => array(
-                'name' => 'mcu-http-serializer', 
-                'options' => array(
-                    'type' => 'direct', 
-                    'passive' => false, 
-                    'durable' => true, 
-                    'autoDelete' => false
+$globalConfig = new \Zend\Config\Config(
+        array(
+            'broker' => array(
+                'connection' => array(
+                    'host' => 'localhost',
+                    'port' => 5672,
+                    'user' => 'mcu',
+                    'password' => 'mcuapi',
+                    'vhost' => '/mcu'
+                ),
+                
+                'bindings' => array(
+                    'exchange' => array(
+                        'name' => 'mcu-http-serializer',
+                        'options' => array(
+                            'type' => 'direct',
+                            'passive' => false,
+                            'durable' => true,
+                            'autoDelete' => false
+                        )
+                    ),
+                    'queue' => array(
+                        'name' => 'rpc-request',
+                        'options' => array(
+                            'passive' => false,
+                            'durable' => true,
+                            'exclusive' => false,
+                            'autoDelete' => true
+                        )
+                    ),
+                    'consumer' => array(
+                        'tag' => 'rpc-broker',
+                        'noLocal' => false,
+                        'noAck' => true,
+                        'exclusive' => true,
+                        'noWait' => false
+                    )
                 )
-            ), 
-            'queue' => array(
-                'name' => 'rpc-request', 
-                'options' => array(
-                    'passive' => false, 
-                    'durable' => true, 
-                    'exclusive' => false, 
-                    'autoDelete' => true
+            ),
+            
+            'handler' => array(
+                'className' => '\HttpSer\Broker\Handler\HttpsRelay',
+                'params' => array(
+                    'targetUrl' => 'https://hroch.cesnet.cz/test/target.php',
+ 
+                    'client' => array(
+                        
+                        'options' => array(
+                            'keepalive' => true,
+                            'useragent' => 'HTTP Serializer'
+                        ),
+                        
+                        'adapter' => array(
+                            'class' => 'Zend\Http\Client\Adapter\Socket',
+                            'options' => array(
+                                'persistent' => true,
+                                'ssltransport' => 'ssl'
+                            ),
+                            'streamContext' => array(
+                                'ssl' => array(
+                                    'verify_peer' => true,
+                                    'cafile' => '/etc/ssl/certs/tcs-ca-bundle.pem',
+                                    'allow_self_signed' => false
+                                )
+                            )
+                        )
+                    )
                 )
-            ), 
-            'consumer' => array(
-                'tag' => 'rpc-broker', 
-                'noLocal' => false, 
-                'noAck' => true, 
-                'exclusive' => true, 
-                'noWait' => false
             )
-        )
-    ), 
-    
-    'handler' => array(
-        'className' => '\HttpSer\Broker\Handler\HttpRelay', 
-        'params' => array(
-            'targetUrl' => 'http://hroch.cesnet.cz/test/target.php'
-        )
-    )
-));
+        ));
 
 $broker = new Broker($globalConfig->broker);
 
